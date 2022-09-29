33d7bc2ab40d9cbfc7c20217580ccc756437ec14
Bykova Kateryna <bykova.kateryna@gmail.com>
2022-09-29 11:27:49 +0300

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,20 +6,19 @@ import { useEffect } from 'react';
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ onClose, children }) => {
-  const handleDownInEscape = e => {
-    if (e.code === 'Escape') {
-      onClose();
-      console.log('close in Escape');
-    }
-  };
-
   useEffect(() => {
+    const handleDownInEscape = e => {
+      if (e.code === 'Escape') {
+        onClose();
+        console.log('close in Escape');
+      }
+    };
     console.log('modal open');
     window.addEventListener('keydown', handleDownInEscape);
     return () => {
       return window.removeEventListener('keydown', handleDownInEscape);
     };
-  });
+  }, [onClose]);
 
   const handleDown = e => {
     if (e.currentTarget === e.target) {
